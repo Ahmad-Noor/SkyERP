5206e127ebda9ceb482d02b5b9336d3c3e77b631
Ahmed Altaher <ahmed_altaher@uhaul.com>
2021-09-03 18:33:32 -0500

    added folders in UI

--- Sky.ERP.API/Program.cs
+++ Sky.ERP.API/Program.cs
@@ -15,8 +15,38 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "Sky.ERP.API", Version = "v1" });
 });
 
-var app = builder.Build();
+//Enable CORS
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
+
+////JSON Serializer
+///Microsoft.AspNetCore.Mvc.NewtonsoftJson
+//builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+//options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+//    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+//    = new DefaultContractResolver());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var app = builder.Build(); 
+
+//Enable CORS
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+ 
 // Configure the HTTP request pipeline.
 if (builder.Environment.IsDevelopment())
 {
@@ -24,6 +54,7 @@ if (builder.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sky.ERP.API v1"));
 }
+
 
 app.UseHttpsRedirection();
 
